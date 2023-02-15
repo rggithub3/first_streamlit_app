7762ca573f020197f0baf6a8b36526511ca2e09b
rggithub3 <125363124+rggithub3@users.noreply.github.com>
2023-02-15 10:27:18 +0530

    Update streamlit_app.py
    added a text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale,Spinach,Rocket Smoothie')
-streamlit.text('Hard Boiled Free-Ranged Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 kale,Spinach,Rocket Smoothie')
+streamlit.text('🐔 Hard Boiled Free-Ranged Egg')
+streamlit.text('🥑🍞 Avacado Toast')
