cf9d7e6f738b7a46fcca7f6d4328631183b4c7f8
rggithub3 <125363124+rggithub3@users.noreply.github.com>
2023-04-01 15:30:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ try:
   if not fruit_choice:
        streamlit.error("please select a fruit to get information.")
   else:
-       back_from_function = grt_fruityvice_data(fruit_choice)
+       back_from_function = get_fruityvice_data(fruit_choice)
        streamlit.dataframe(back_from_function)
 
 
