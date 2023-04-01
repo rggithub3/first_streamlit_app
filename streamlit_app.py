9762ed6d9c3197a9f7b10dcee263aa66aa8ddefd
rggithub3 <125363124+rggithub3@users.noreply.github.com>
2023-04-01 13:04:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ streamlit.dataframe(my_data_rows)
 
 
 add_my_friut = streamlit.text_input('What fruit would you like to add')
-streamlit.write('Thanks for adding', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
